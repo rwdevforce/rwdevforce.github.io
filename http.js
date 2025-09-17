88faf308f5f1de01856d5a49e36c5f09e0dc2f19
DevForce <rwdevforce@gmail.com>
2025-09-17 04:44:04 +0300

    Update http.js

--- http.js
+++ http.js
@@ -1,10 +1,44 @@
-var test;
+var http;
     $.ajax({
         url: "https://raw.githubusercontent.com/74647/Proxify/main/proxies/http.txt",
-        dataType: "text",
+        dataType: "http",
         async: true,
         success: function(msg){
             test = msg;
-            alert('Содержимое файла: '+test);
+            alert('HTTP: '+http);
         }
     });
+
+var https:
+    $.ajax({
+        url: "https://raw.githubusercontent.com/74647/Proxify/main/proxies/https.txt",
+        dataType: "https",
+        async: true,
+        success: function(msg){
+            test = msg;
+            alert('HTTPS: '+https);
+        }
+    });
+
+var socks4:
+    $.ajax({
+        url: "https://raw.githubusercontent.com/74647/Proxify/main/proxies/socks4.txt",
+        dataType: "socks4",
+        async: true,
+        success: function(msg){
+            test = msg;
+            alert('SOCKS4: '+socks4);
+        }
+    });
+
+var socks5;
+    $.ajax({
+        url: "https://raw.githubusercontent.com/74647/Proxify/main/proxies/socks5.txt",
+        dataType: "socks5",
+        async: true,
+        success: function(msg){
+            test = msg;
+            alert('SOCKS5: '+socks5);
+        }
+    });
+
